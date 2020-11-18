6eabb8e7cb26d478e8127247aaefc9aac7df19f6
Trevin Wilkinson <trevin.wilkinson@gmail.com>
2020-11-17 18:41:39 -0800

    work on tagging feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -50,6 +50,6 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :tag_list)
   end
 end
